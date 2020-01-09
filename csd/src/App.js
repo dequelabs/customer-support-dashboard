3a0146e9f2584974e1e48353bf39ab64743ad44e
jthickens <47118049+jthickens@users.noreply.github.com>
2020-01-09 16:03:34 -0500

    skeleton for landing page complete

--- csd/src/App.js
+++ csd/src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import './App.css';
-import logo from './assets/dq_2nd_logo.png';
+import 'typeface-roboto';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, Row, Col, Button} from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
+      <div className="App-Nav">
+        <button className="Esc-Btn" type="button">Home</button>
+      </div>
       <header className="App-header">
-        <p>
           Welcome to the Deque Customer Support Center!
-        </p>
       </header>
-      <p className="logo" id="html">
-          <img src={logo} />
-        </p>
+      <div className="App-body">
+        <button className="Nav-Btn" type="button">Submit Request</button>
+        <button className="Nav-Btn" type="button">View Request</button>      
+      </div>     
     </div>
   );
 }
 
-
-
 export default App;
+
+
---
+++ csd/src/components/submitRequest.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'typeface-roboto';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Container, Row, Col, Button} from "react-bootstrap";
+
+
+function submitRequest() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
