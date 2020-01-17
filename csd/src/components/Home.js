8e053ff38cac0370b66a6e6a7ae6b9a291ee7892
jthickens <47118049+jthickens@users.noreply.github.com>
2020-01-17 16:24:02 -0500

    Beginning of (hopefully) production app. Includes Deque brand reskin.

--- csd/src/components/Home.js
+++ csd/src/components/Home.js
@@ -1,15 +1,13 @@
 import React, { Component }from 'react';
-import { Button, } from 'cauldron-react';
-import { Link, } from "react-router-dom";
+import { Button, Link, } from 'cauldron-react';
+import Grid from '@material-ui/core/Grid';
+
 import '../App.css';
-
-
-
 
 export default class Home extends Component {
 
     componentDidMount() {
-        document.title = "Home - Deque Customer Support";
+        document.title = "Support | Deque Systems";
     }
 
     constructor(props) {
@@ -24,22 +22,69 @@ export default class Home extends Component {
 
         return (
             <div className="App">
-                <div className="App-Nav">
-                    <Link to="/home">
-                        <Button className="Esc-Btn" type="button">Home</Button>
-                    </Link> 
+                <div className='Header'>
+                    <a href="http://www.deque.com" className='HomeLink'>
+                        <img src="https://accessibility.deque.com/hubfs/logo-white.svg" width="100" alt="Deque" title="Deque"></img>
+                    </a>
                 </div>
-                <h1 className="App-header">
-                    Welcome to the Deque Customer Support Center!
-                </h1>
-                <div className="App-body">
-                    <Link to="/request">
-                        <Button className="Esc-Btn" type="button">Submit Request</Button>
-                    </Link> 
-                    <Link to="/view">
-                        <Button className="Esc-Btn" type="button">View Requests</Button>
-                    </Link> 
-                </div>     
+                <body className='Col'>
+                    <Grid container spacing={1}>
+                        <Grid item xs={5}>
+                            <div>
+                                <h1>
+                                    Deque Customer Support
+                                </h1>
+                                <p>
+                                    Welcome to the Deque Customer support center. Here, you can raise a new customer support request, view the status of your pending requests, and modify any submitted requests
+                                </p>
+                                <p>
+                                    Contact Support with any product questions you have or issues you encounter. Our Deque Support team will respond within 1 working day of receiving your request.
+                                </p>
+                                <h2>
+                                    You can raise a Customer Support Request at the link below
+                                </h2>
+                                <Button>Make A Request</Button>
+                            </div>
+                        </Grid>
+                        <Grid item xs={7}>
+                            <div>
+                                <h2>
+                                    Open Requests
+                                </h2>
+                            </div>
+                            <Grid container spacing={2}>
+                                <Grid item xs={4}>
+                                    <p>
+                                        summary
+                                    </p>
+                                </Grid>
+                                <Grid item xs={2}>
+                                    <p>
+                                        status
+                                    </p>
+                                </Grid>
+                                <Grid item xs={2}>
+                                    <p>
+                                        type
+                                    </p>
+                                </Grid>
+                                <Grid item xs={2}>
+                                    <p>
+                                        date
+                                    </p>
+                                </Grid>
+                                <Grid item xs={2}>
+                                    <p>
+                                        requester
+                                    </p>
+                                </Grid>
+                            </Grid>
+                            <div>
+                                loc of 'issues' react component (list of issues)
+                            </div>
+                        </Grid>
+                    </Grid>
+                </body>
             </div>
         );
     }
