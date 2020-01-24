a4383d971a7f58eb6000a1e131dbd7ca37b1e4d1
jthickens <47118049+jthickens@users.noreply.github.com>
2020-01-24 16:31:45 -0500

    Demo-Ready Home & Detail View Pages

--- csd/src/components/DetailView.js
+++ csd/src/components/DetailView.js
@@ -1,7 +1,8 @@
 import React, { Component }from 'react';
-import { TopBar, Workspace,} from 'cauldron-react';
+import { TopBar, Workspace, TextField} from 'cauldron-react';
 import Grid from '@material-ui/core/Grid';
 import '../App.css';
+import '../styles/detailView.css'
 import { get, } from '../services/api';
 import IssueType from './IssueType';
 
@@ -42,9 +43,9 @@ export default class DetailView extends Component {
         let key = 0;
         if(this.state.issue.requestFieldValues[3].value !== '') {
             details.push(
-                <p className='BodyText' key={key++}>
+                <p className='Descriptor Text' key={key++}>
                     Product <br/>
-                    <span>
+                    <span className='Content Text'>
                         {this.state.issue.requestFieldValues[3].value}
                     </span>
                 </p>
@@ -52,9 +53,9 @@ export default class DetailView extends Component {
         }
         if(this.state.issue.requestFieldValues[1].value !== '') {
             details.push(
-                <p className='BodyText' key={key++}>
+                <p className='Descriptor Text' key={key++}>
                     Description <br/>
-                    <span>
+                    <span className='Content Text'>
                         {this.state.issue.requestFieldValues[1].value}
                     </span>
                 </p>
@@ -63,9 +64,9 @@ export default class DetailView extends Component {
         if(this.state.issue.requestFieldValues[2].value !== '') {
             console.log()
             details.push(
-                <p className='BodyText' key={key++}>
+                <p className='Descriptor Text' key={key++}>
                     Additional Info <br/>
-                    <span>
+                    <span className='Content Text'>
                         {this.state.issue.requestFieldValues[2].value}
                     </span>
                 </p>
@@ -79,9 +80,9 @@ export default class DetailView extends Component {
         let key = 0;
         this.state.issueComments.forEach(element => {
             comments.push(
-                <p className='BodyText' key={key++}>
-                    {element.author.displayName} {element.created.friendly}<br/>
-                    <span>
+                <p  className='Descriptor Text'key={key++}>
+                    {element.author.displayName} - {element.created.friendly}<br/>
+                    <span className='Content Text'>
                         {element.body}
                     </span>
                 </p>
@@ -117,43 +118,85 @@ export default class DetailView extends Component {
         //else, ticket found. render detail page
         } else {
             return (
-            <div className="App">
+            <div >
                 <TopBar className='Header'>
                     <a href="http://www.deque.com" className='HomeLink'>
                         <img src="https://accessibility.deque.com/hubfs/logo-white.svg" width="100" alt="Link to Deque Home" title="Deque"></img>
                     </a>
                 </TopBar>
-                <Workspace className='Col'>
-                    <Grid container spacing={1} >
+                <Workspace className='DetailPage'>
+                    <Grid container spacing={0} >
                         <Grid item xs={12} md={8}>
                                 <h1 className='HeadText'>
                                     {this.state.issue.requestFieldValues[0].value}
                                 </h1>
-                                <p className='BodyText'>
-                                    Request raised by {this.state.issue.reporter.displayName} {this.state.issue.createdDate.friendly}
-                                </p>
-                                <div>
-                                    {this.detailBuilder()}
+                                <div className='Details'>
+                                    <p className='Content Text'>
+                                        <span className='Descriptor Text'>{this.state.issue.reporter.displayName} </span> 
+                                        Raised This
+                                        <span className='Descriptor Text'> {this.state.issue.createdDate.friendly}</span>
+                                    </p>
+                                    <div >
+                                        {this.detailBuilder()}
+                                    </div>
                                 </div>
+                                
                                 <h2 className='SecondHead'>
                                     Follow Up:
                                 </h2>
-                                <div>
+                                <div className='Comments'>
                                     {this.commentsBuilder()}
                                 </div>
+                                <form>
+                                    <TextField 
+                                        multiline 
+                                        label="Add Comment" 
+                                        className='CommentInput'
+                                    />
+                                    <button type='submit' className='SaveButton Text'>Save</button>
+                                </form>
                         </Grid>
                         <Grid item xs={12} md={4}>
                             <h2 className='SecondHead'>
                                 Status
                             </h2>
-                            
-                            <div className='BodyText'>
-                                <p>Request type: <IssueType type={this.state.issue.requestTypeId}/></p>
-                                <p>Status: {this.state.issue.currentStatus.status}</p>
-                                <p>Date Created: {this.state.issue.createdDate.friendly}</p>
-                                <p>Shared With: {this.state.issue.reporter.displayName}</p>
-                                <p>Notification Status: Subscribed</p>
-                                <p>Issue Key: {this.state.issue.issueKey}</p>
+                            <div >
+                                <p className='Descriptor Text'>
+                                    Request type<br/> 
+                                    <span className='Content Text'>
+                                        <IssueType type={this.state.issue.requestTypeId}/>
+                                    </span>
+                                </p>
+                                <p className='Descriptor Text'>
+                                    Status<br/> 
+                                    <span className='Content Text'>
+                                        {this.state.issue.currentStatus.status}
+                                    </span>
+                                </p>
+                                <p className='Descriptor Text'>
+                                    Date Created<br/>
+                                    <span className='Content Text'>{
+                                    this.state.issue.createdDate.friendly}
+                                    </span>
+                                </p>
+                                {/* <p className='Descriptor Text'>
+                                    Shared With<br/>
+                                        <span className='Content Text'>
+                                            {this.state.issue.reporter.displayName}
+                                        </span>
+                                    </p>
+                                <p className='Descriptor Text'>
+                                    Notification Status<br/>
+                                    <span className='Content Text'>
+                                        Subscribed
+                                    </span>
+                                </p> */}
+                                <p className='Descriptor Text'>
+                                    Issue Key<br/> 
+                                    <span className='Content Text'>
+                                        {this.state.issue.issueKey}
+                                    </span>
+                                </p>
                             </div>
                         </Grid>
                     </Grid>
